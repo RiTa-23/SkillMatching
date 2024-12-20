ab8feb0e835a94bdbcdc6461e76d2570648b26e3
U5 <kyutech.joko@gmail.com>
2024-12-20 22:08:26 +0900

    change:login->signin#30

--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -1,11 +1,11 @@
-import LoginForm from "../../components/login/LoginForm";
+import SigninForm from "../../components/login/SigninForm";
 
 const LoginPage = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <div className="flex flex-col justify-center items-center w-1/2 max-w-[400px] bg-white p-12 rounded shadow-md">
         <h2 className="text-3xl">ログイン</h2>
-        <LoginForm />
+        <SigninForm />
       </div>
     </div>
   );
