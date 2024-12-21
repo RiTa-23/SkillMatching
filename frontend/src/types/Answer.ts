bf4b34da3ca64babd66ac55c667deae9ae6f2c8a
U5 <kyutech.joko@gmail.com>
2024-12-21 18:50:21 +0900

    add:answer type#35

---
+++ frontend/src/types/Answer.ts
@@ -0,0 +1,10 @@
+export interface Answer {
+  id: number;
+  userId: number;
+  questionId: number;
+  answer: string;
+  responseValue: number;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+}
