e57fd5dfc3cfb910ce25aaf8629ce29d49646e97
U5 <kyutech.joko@gmail.com>
2024-12-21 20:16:03 +0900

    add:shadcn#33

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,44 +1,86 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        main: {
-          "50": "#effaf4",
-          "100": "#d8f3e2",
-          "200": "#b3e7c9",
-          "300": "#82d3aa",
-          "400": "#4eb987",
-          "500": "#2b9e6c",
-          "600": "var(--main-color)",
-          "700": "#176547",
-          "800": "#14513a",
-          "900": "#124230",
-          "950": "#09251b",
-        },
-        accent: {
-          "50": "#fafbeb",
-          "100": "#f5f6cb",
-          "200": "#eeee9a",
-          "300": "#e4de60",
-          "400": "#dacc35",
-          "500": "var(--accent-color)",
-          "600": "#af901f",
-          "700": "#8c6b1c",
-          "800": "#75561e",
-          "900": "#64481f",
-          "950": "#3a260e",
-        },
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			main: {
+  				'50': '#effaf4',
+  				'100': '#d8f3e2',
+  				'200': '#b3e7c9',
+  				'300': '#82d3aa',
+  				'400': '#4eb987',
+  				'500': '#2b9e6c',
+  				'600': 'var(--main-color)',
+  				'700': '#176547',
+  				'800': '#14513a',
+  				'900': '#124230',
+  				'950': '#09251b'
+  			},
+  			accent: {
+  				'50': '#fafbeb',
+  				'100': '#f5f6cb',
+  				'200': '#eeee9a',
+  				'300': '#e4de60',
+  				'400': '#dacc35',
+  				'500': 'var(--accent-color)',
+  				'600': '#af901f',
+  				'700': '#8c6b1c',
+  				'800': '#75561e',
+  				'900': '#64481f',
+  				'950': '#3a260e',
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
