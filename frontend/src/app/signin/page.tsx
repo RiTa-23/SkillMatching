f58f55e300a9126bec10f0cbea0f926a78ab9887
U5 <kyutech.joko@gmail.com>
2024-12-21 18:19:06 +0900

    change:header

--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -3,7 +3,7 @@ import SigninForm from "@/components/login/SigninForm";
 
 const SigninPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center h-[90vh]">
       <div className="flex flex-col justify-center items-center w-1/2 max-w-[400px] bg-white p-12 rounded shadow-md">
         <h2 className="text-3xl">ログイン</h2>
         <SigninForm />
--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -3,7 +3,7 @@ import SignupForm from "@/components/login/SignupForm";
 
 const SignupPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center h-[90vh]">
       <div className="flex flex-col justify-center items-center w-1/2 max-w-[400px] bg-white p-12 rounded shadow-md">
         <h2 className="text-3xl">新規登録</h2>
         <SignupForm />
