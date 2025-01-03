066a26f6bb68375a55a84f32776a9b1ea1bf4e96
U5 <kyutech.joko@gmail.com>
2025-01-03 17:31:21 +0900

    update:questionの型変更(front)#75

--- frontend/src/types/Question.ts
+++ frontend/src/types/Question.ts
@@ -1,7 +1,8 @@
 export interface Question {
-  id: number;
-  category?: string;
-  language?: string;
+  question_id: number;
+  company_id: string;
+  category_id?: string;
+  language_id?: string;
   question: string;
   createdAt?: string;
   updatedAt?: string;
