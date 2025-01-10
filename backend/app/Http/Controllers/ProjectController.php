756d087cc2880771c1625f30d8c363fee4450c88
U5 <kyutech.joko@gmail.com>
2025-01-10 23:10:00 +0900

    update:project_id,title,statusのみ取得#48

--- backend/app/Http/Controllers/ProjectController.php
+++ backend/app/Http/Controllers/ProjectController.php
@@ -9,16 +9,11 @@ class ProjectController extends Controller
 {
     public function getProjects()
     {
-        $projects = Project::with('company')
-            ->get()
+        $projects = Project::get()
             ->map(function ($project) {
                 return [
                     'project_id' => $project->project_id,
                     'title' => $project->title,
-                    'company_name' => $project->company->company_name,
-                    'contents' => $project->contents,
-                    'start_date' => $project->start_date,
-                    'end_date' => $project->end_date,
                     'status' => $project->status,
                 ];
             });
