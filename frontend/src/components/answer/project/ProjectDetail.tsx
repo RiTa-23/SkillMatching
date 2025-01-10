d128b151908c9908253682966148554f27fa1e0a
U5 <kyutech.joko@gmail.com>
2025-01-11 01:20:57 +0900

    update:案件詳細方法変更

--- frontend/src/components/answer/project/ProjectDetail.tsx
+++ frontend/src/components/answer/project/ProjectDetail.tsx
@@ -1,94 +1,28 @@
-"use client";
-
-import { useState } from "react";
 import Link from "next/link";
 
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import { TableCell, TableRow } from "@/components/ui/table";
 
-import Cookies from "js-cookie";
-import fetcher from "@/lib/fetcher";
 import type { Project } from "@/types/Project";
-import type { ProjectDetail } from "@/types/Project";
 
 interface ProjectProps {
   project: Project;
 }
 
-const ProjectDetail = (props: ProjectProps) => {
-  const { project } = props;
-  const [projectDetail, setProjectDetail] = useState<ProjectDetail | null>(
-    null
-  );
-
-  const fetchData = async (token: string) => {
-    const { data, error } = await fetcher<ProjectDetail>({
-      url: `project/${project.project_id}`,
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    if (data) {
-      setProjectDetail(data);
-    }
-    if (error) {
-      console.error(error);
-    }
-  };
-
-  const getProjectDetail = () => {
-    const token = Cookies.get("token");
-    if (token) {
-      fetchData(token);
-    }
-  };
-
+const ProjectDetail = ({ project }: ProjectProps) => {
   return (
-    <Dialog>
-      <TableRow key={project.project_id} className="hover:bg-gray-100">
-        <TableCell>
-          <DialogTrigger onClick={getProjectDetail}>
-            {project.project_id}
-          </DialogTrigger>
-        </TableCell>
-        <TableCell>
-          <DialogTrigger onClick={getProjectDetail}>
-            {project.title}
-          </DialogTrigger>
-        </TableCell>
-        <TableCell>
-          <DialogTrigger onClick={getProjectDetail}>
-            {project.status}
-          </DialogTrigger>
-        </TableCell>
-      </TableRow>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>{project.title}</DialogTitle>
-        </DialogHeader>
-        <div>
-          <p>開始日: {projectDetail?.start_date}</p>
-          <p>終了日: {projectDetail?.end_date}</p>
-          <p>ステータス: {projectDetail?.status}</p>
-          <p>説明: {projectDetail?.contents}</p>
-          <Link href={`./project/${project.project_id}/`}>
-            <div
-              role="button"
-              className="inline-flex items-center rounded-md text-sm font-medium bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 mt-2"
-            >
-              評価
-            </div>
-          </Link>
-        </div>
-      </DialogContent>
-    </Dialog>
+    <TableRow className="hover:bg-gray-100">
+      <TableCell>
+        <Link href={`./project/${project.project_id}`}>
+          {project.project_id}
+        </Link>
+      </TableCell>
+      <TableCell>
+        <Link href={`./project/${project.project_id}`}>{project.title}</Link>
+      </TableCell>
+      <TableCell>
+        <Link href={`./project/${project.project_id}`}>{project.status}</Link>
+      </TableCell>
+    </TableRow>
   );
 };
 
--- frontend/src/components/answer/project/ProjectEvaluation.tsx
+++ frontend/src/components/answer/project/ProjectEvaluation.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -16,6 +17,8 @@ import {
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Textarea } from "@/components/ui/textarea";
 
+import type { ProjectDetail } from "@/types/Project";
+
 const EvaluationSchema = z.object({
   evaluation: z.enum(["1", "2", "3", "4", "5"], {
     required_error: "1~5のいずれかを選択してください",
@@ -25,7 +28,11 @@ const EvaluationSchema = z.object({
 
 export type EvaluationFormValues = z.infer<typeof EvaluationSchema>;
 
-const ProjectEvaluation = () => {
+interface ProjectEvaluationProps {
+  project: ProjectDetail;
+}
+
+const ProjectEvaluation = ({ project }: ProjectEvaluationProps) => {
   const form = useForm<EvaluationFormValues>({
     resolver: zodResolver(EvaluationSchema),
   });
@@ -37,6 +44,13 @@ const ProjectEvaluation = () => {
   return (
     <Card className="w-[80%] max-w-[800px] p-8 mt-10">
       <CardContent>
+        <ul>
+          <li>{project.title}</li>
+          <li>{project.contents}</li>
+          <li>{project.start_date}</li>
+          <li>{project.end_date}</li>
+          <li>{project.status}</li>
+        </ul>
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
@@ -54,36 +68,11 @@ const ProjectEvaluation = () => {
                       defaultValue={field.value}
                       className="flex space-x-4"
                     >
-                      <FormItem className="flex items-center space-x-3 space-y-0">
-                        <FormControl>
-                          <RadioGroupItem value="1" />
-                        </FormControl>
-                        <FormLabel className="font-normal">1</FormLabel>
-                      </FormItem>
-                      <FormItem className="flex items-center space-x-3 space-y-0">
-                        <FormControl>
-                          <RadioGroupItem value="2" />
-                        </FormControl>
-                        <FormLabel className="font-normal">2</FormLabel>
-                      </FormItem>
-                      <FormItem className="flex items-center space-x-3 space-y-0">
-                        <FormControl>
-                          <RadioGroupItem value="3" />
-                        </FormControl>
-                        <FormLabel className="font-normal">3</FormLabel>
-                      </FormItem>
-                      <FormItem className="flex items-center space-x-3 space-y-0">
-                        <FormControl>
-                          <RadioGroupItem value="4" />
-                        </FormControl>
-                        <FormLabel className="font-normal">4</FormLabel>
-                      </FormItem>
-                      <FormItem className="flex items-center space-x-3 space-y-0">
-                        <FormControl>
-                          <RadioGroupItem value="5" />
-                        </FormControl>
-                        <FormLabel className="font-normal">5</FormLabel>
-                      </FormItem>
+                      <RadioGroupItem value="1" />
+                      <RadioGroupItem value="2" />
+                      <RadioGroupItem value="3" />
+                      <RadioGroupItem value="4" />
+                      <RadioGroupItem value="5" />
                     </RadioGroup>
                   </FormControl>
                   <FormMessage />
