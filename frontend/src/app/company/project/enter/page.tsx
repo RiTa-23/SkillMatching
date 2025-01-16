5cbea3e25ec593e4370537d7d06c3389aed8d9b7
RiTa-23 <rita.tifrt@gmail.com>
2025-01-16 22:16:47 +0900

    案件入力画面フロント作成完了

--- frontend/src/app/company/project/enter/page.tsx
+++ frontend/src/app/company/project/enter/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import React from 'react';
-import { useForm, Controller } from 'react-hook-form';
+import { useForm, FormProvider,Controller } from 'react-hook-form';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Form, FormField, FormItem, FormLabel, FormControl } from '@/components/ui/form';
 import { Button } from '@/components/ui/button';
 
 const ProjectInputPage = () => {
-  const { control, handleSubmit } = useForm({
+  const methods = useForm({
     defaultValues: {
       companyName: '',
       projectName: '',
@@ -27,74 +27,76 @@ const ProjectInputPage = () => {
   };
 
   return (
+    <FormProvider {...methods}>
     <div className="container mx-auto p-6">
       <Card className="p-6">
         <h1 className="text-2xl font-bold mb-4">案件入力</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <FormField name="companyName" control={control}>
-            <FormItem>
-              <FormLabel>案件元会社名</FormLabel>
-              <FormControl>
-                <Controller
-                  name="companyName"
-                  control={control}
-                  render={({ field }) => <Input {...field} />}
-                />
-              </FormControl>
-            </FormItem>
-          </FormField>
-          <FormField name="projectName" control={control}>
-            <FormItem>
-              <FormLabel>案件名</FormLabel>
-              <FormControl>
-                <Controller
-                  name="projectName"
-                  control={control}
-                  render={({ field }) => <Input {...field} />}
-                />
-              </FormControl>
-            </FormItem>
-          </FormField>
-          <FormField name="projectDetail" control={control}>
-            <FormItem>
-              <FormLabel>案件詳細</FormLabel>
-              <FormControl>
-                <Controller
-                  name="projectDetail"
-                  control={control}
-                  render={({ field }) => <Input {...field} />}
-                />
-              </FormControl>
-            </FormItem>
-          </FormField>
-          <FormField name="members" control={control}>
-            <FormItem>
-              <FormLabel>メンバー (カンマ区切り)</FormLabel>
-              <FormControl>
-                <Controller
-                  name="members"
-                  control={control}
-                  render={({ field }) => <Input {...field} />}
-                />
-              </FormControl>
-            </FormItem>
-          </FormField>
-          <FormField name="technologies" control={control}>
-            <FormItem>
-              <FormLabel>使用技術 (カンマ区切り)</FormLabel>
-              <FormControl>
-                <Controller
-                  name="technologies"
-                  control={control}
-                  render={({ field }) => <Input {...field} />}
-                />
-              </FormControl>
-            </FormItem>
-          </FormField>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
+          <FormField 
+            name="companyName"
+            control={methods.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>案件元会社名</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField 
+            name="projectName"
+            control={methods.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>案件名</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField 
+            name="projectDetail"
+            control={methods.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>案件詳細</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField 
+            name="members"
+            control={methods.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>メンバー (カンマ区切り)</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField 
+            name="technologies"
+            control={methods.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>使用技術 (カンマ区切り)</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
           <Button type="submit">送信</Button>
         </form>
       </Card>
     </div>
+    </FormProvider>
   );
 };
 
