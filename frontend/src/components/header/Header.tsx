f6d0f5387b86e94ac5b0ba59a4b782da1be755e8
U5 <kyutech.joko@gmail.com>
2025-01-17 13:37:48 +0900

    change:logo#116

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import {
   NavigationMenu,
@@ -12,10 +13,16 @@ interface HeaderProps {
 
 const Header = ({ urls }: HeaderProps) => {
   return (
-    <header className="flex justify-between items-center bg-main-600 h-[15vh]">
-      <h1 className="text-accent-500 text-6xl font-extrabold ml-6">
-        Skill Matching
-      </h1>
+    <header className="flex justify-between items-center bg-main-600 h-[10vh]">
+      <Link href="/" passHref>
+        <Image
+          src="/logo.png"
+          alt="logo"
+          width={400}
+          height={80}
+          className="cursor-pointer"
+        />
+      </Link>
       <NavigationMenu>
         <NavigationMenuList className="space-x-10 text-white text-lg mr-6">
           {urls.map((url) => (
